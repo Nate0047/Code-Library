164513032472d3398009d39b1cb53951bdf4efbc
Nate0047 <93774120+Nate0047@users.noreply.github.com>
2022-04-11 14:03:33 +0100

    Add files via upload

---
+++ New Script Setup.R
@@ -0,0 +1,10 @@
+# LIBRARY AND PACKAGES ---------------------------------------------------------
+.libPaths("C:/R library")
+
+if(!require("tidyverse")) {install.packages("tidyverse")}
+library(tidyverse)
+if(!require("summarytools")) {install.packages("summarytools")}
+library(summarytools)
+
+
+# IMPORT DATA ------------------------------------------------------------------
